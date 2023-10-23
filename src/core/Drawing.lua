d7233074df6bd829073c724f735a0b2597460529
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-10-23 16:38:38 +0200

    Account for statusbar height in scale calculation

--- src/core/Drawing.lua
+++ src/core/Drawing.lua
@@ -7,7 +7,7 @@ Drawing = {
 
 function Drawing.size_up()
     Drawing.initial_size = wgui.info()
-    Drawing.scale = Drawing.initial_size.height / 600
+    Drawing.scale = (Drawing.initial_size.height - 23) / 600
     local extra_space = (Settings.grid_size * 8)
     if Drawing.scale > 1 + Drawing.scale_tolerance or Drawing.scale < 1 - Drawing.scale_tolerance then
         extra_space = extra_space * Drawing.scale
