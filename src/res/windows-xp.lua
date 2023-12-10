70dfa0decb3aee1e2574f7f223be03aba9d889ac
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-12-10 15:23:29 +0100

    Fix Windows XP theme font size

--- src/res/windows-xp.lua
+++ src/res/windows-xp.lua
@@ -2,6 +2,7 @@ local theme = get_base_style()
 
 theme.path = res_path .. "windows-xp-atlas.png"
 theme.background_color = { r = 236, g = 233, b = 216 }
+theme.font_size = 11.4
 theme.font_name = "Tahoma"
 theme.pixelated_text = true
 
