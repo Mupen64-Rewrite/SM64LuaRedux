184474af0007804951afde2e9e5b6baf30fec786
Alex <48759429+Aurumaker72@users.noreply.github.com>
2024-01-06 23:04:08 +0100

    Use Consolas font in Crackhex theme

--- src/res/crackhex/style.lua
+++ src/res/crackhex/style.lua
@@ -1,5 +1,6 @@
 local theme = get_base_style()
 
+theme.font_name = "Consolas"
 theme.background_color = BreitbandGraphics.repeated_to_color(34)
 theme.button.text_colors = {
     [1] = BreitbandGraphics.hex_to_color("#F7A8B8"),
