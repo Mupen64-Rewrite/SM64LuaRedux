046b5f0e736588ab94511df50c3114148a1abbfd
Frame <nafoizret@web.de>
2024-10-26 23:21:57 +0200

    move save/load buttons to the 'ToolsGui' section

--- src/views/PianoRoll/ToolsGui.lua
+++ src/views/PianoRoll/ToolsGui.lua
@@ -20,5 +20,46 @@ return {
         }) then
             PianoRollContext.current = PianoRoll.new()
         end
+
+        if PianoRollContext.current ~= nil then
+            if (ugui.button({
+                uid = UID.SavePianoRoll,
+
+                rectangle = grid_rect(2, top, 1, 0.5),
+                is_enabled = true,
+                text = 'Save'
+            })) then
+                ugui.end_frame()
+                local filePath = iohelper.filediag("*.pianoroll", 1)
+                if filePath ~= "" then
+                    persistence.store(filePath,
+                        {
+                            frames      = PianoRollContext.current.frames,
+                            name        = PianoRollContext.current.name,
+                            startGT     = PianoRollContext.current.startGT,
+                            endGT       = PianoRollContext.current.endGT,
+                            editingGT   = PianoRollContext.current.editingGT,
+                            previewGT   = PianoRollContext.current.previewGT,
+                        }
+                    )
+                end
+            end
+
+            if (ugui.button({
+                uid = UID.LoadPianoRoll,
+
+                rectangle = grid_rect(3, top, 1, 0.5),
+                is_enabled = true,
+                text = 'Load'
+            })) then
+                ugui.end_frame()
+                local filePath = iohelper.filediag("*.pianoroll", 0)
+                local contents = persistence.load(filePath);
+                if contents ~= nil then
+                    CloneInto(PianoRollContext.current, contents)
+                    PianoRollContext.current:jumpTo(PianoRollContext.current.previewGT)
+                end
+            end
+        end
     end,
 }
