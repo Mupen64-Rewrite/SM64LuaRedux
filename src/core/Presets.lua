3f6d3394bce503b91a696df3842a77ce56f10b03
Aurumaker72 <armkdev72@gmail.com>
2025-02-07 20:54:44 +0100

    Presets/Chg: Add interim hack which allows persisting the movement mode

--- src/core/Presets.lua
+++ src/core/Presets.lua
@@ -22,10 +22,18 @@ function Presets.get_default_preset()
 end
 
 function Presets.apply(i)
+    -- HACK: The movement mode (which is also used by the TAS tab) is stored in TASState, which isn't serialized.
+    -- We do however need to serialize the movement mode for UX purposes.
+    -- Here, the movement mode is injected into the presets and is also restored when loading
+    Settings.hack_movement_mode = TASState.movement_mode
+
     Presets.persistent.current_index = ugui.internal.clamp(i, 1, #Presets.persistent.presets)
     Settings = Presets.persistent.presets[Presets.persistent.current_index]
     Styles.update_style()
     VarWatch_update()
+
+    -- HACK: See above
+    TASState.movement_mode = Settings.hack_movement_mode == nil and 1 or Settings.hack_movement_mode
 end
 
 function Presets.reset(i)
